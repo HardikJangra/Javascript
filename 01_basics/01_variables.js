ad619a77937b8a2a365a74223581404518054af7
Hardik <hardikjangra2003@gmail.com>
2026-06-19 16:24:06 +0530

    my first js code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountID=    "1234567890";
+let accountEmail=   "hardik@example.com";
+var accountPassword= "mySuperSecretPassword";
+accountCity=       "Mumbai";
+
+accountEmail= "hj@example.com";
+accountpassword= "myEvenMoreSuperSecretPassword";
+accountCity= "Pune";
+let accountState;
+
+console.log(accountID);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
