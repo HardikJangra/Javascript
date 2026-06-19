142d24dc563be407d036a4801d33081dc7e8d0ae
Hardik <hardikjangra2003@gmail.com>
2026-06-19 16:50:21 +0530

    conversionofTypes

---
+++ 03_conversionoperations.js
@@ -0,0 +1,15 @@
+let score = null
+console. log (typeof score); console. log (typeof (score));
+let valueInNumber = Number(score)
+console. log (typeof valueInNumber);
+console. log(typeof valueInNumber);
+console. log (valueInNumber);
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console. log (booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console. log (stringNumber);
+console. log (typeof stringNumber);
