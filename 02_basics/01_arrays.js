89fc5a763b54dcab162eff1a540a835ab0f62dfb
Hardik <hardikjangra2003@gmail.com>
2026-06-23 05:33:07 +0530

    arrays part 2

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+const myArr = [0, 1, 2, 3, 4,5]
+const myHeors = ["shaktiman", "naagraj"]
+const myArI2 = new Array(1, 2, 3, 4)
+console.log (myArr[1]);
+
+// myArI.shift()
+// console. log(myArr.includes(9));
+// console. log (myArI.indexOf(3));
+const newArr = myArr.join()
+console.log (myArr);
+console. log (typeof newArr);
+
+console. log (myn1);
+console. log("B ", myArr) ;
+const myn2 = myArr. splice(1, 3)
+console. log("C ", myArr) ;
+console. log (myn2);
