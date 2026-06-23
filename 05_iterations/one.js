2a183c283541325f134c46007e4f4cfb4f03127c
Hardik <hardikjangra2003@gmail.com>
2026-06-23 16:42:08 +0530

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,11 @@
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+for(let i = 0; i < 10; i++){
+    for(let j = 0; j < 10; j++){
+        console.log(i, j);
+    }
+}
+
+//break statement is used to exit a loop before it has completed all its iterations. It can be used in both for and while loops. When the break statement is executed, the loop will terminate immediately, and control will move to the next statement after the loop.   
